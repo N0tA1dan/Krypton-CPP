9ae1761aead9861bb113b37b55de27560adc9033
Aidan <71911942+N0tA1dan@users.noreply.github.com>
2021-11-14 21:47:43 -0800

    Update krypton.cpp

--- src/krypton.cpp
+++ src/krypton.cpp
@@ -2,20 +2,20 @@
 #include <string.h>
 using namespace std;
 
-int check(string usrinp){
+
+int check(string usrinp, string key){
+    // checks if user length is divisible by 16 evenly. If not then it will pad it with 0's until it is divisble by 16.
     if(usrinp.length() % 16 != 0){
-        for(int i=0;i<usrinp.length();i++){
-            if(usrinp.length() > 16){
-                usrinp.append(15 - usrinp.length(), '0');
-   }else{
-        usrinp.append(16 - usrinp.length(), '0');
-   } 
-  }
- }
+        int pad = 16 * (usrinp.length() / 16 + 1) - usrinp.length();
+        usrinp.append(pad, '0');
+    }
+
+    // This is how many rounds the encryption will preform
+    int round = usrinp.length() / 16;
     cout << usrinp;
     return 0;
 }
 
 int main(){
-    check("11111111111111111");
+    check("Hello World", "bruh");
 }
